c82b49b71d789ac8adc955dbb43d0fc020943e77
AmnaHussien <amnahussin88@gmail.com>
2024-12-03 01:45:09 -0800

    ds

---
+++ website/models.py
@@ -0,0 +1,19 @@
+from website import db
+from flask_login import UserMixin
+from datetime import datetime
+
+#designing data model
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique = True, nullable = False)
+    password = db.Column(db.Integer, unique = True, nullable = False)
+    events = db.relationship('Event')
+    #Task=> Event
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(60))
+    due_date = db.Column(db.DateTime, default=datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    
---
+++ website/views.py
@@ -0,0 +1,59 @@
+from flask_login import login_required, current_user
+from website import db
+from flask import Blueprint, render_template,request, redirect, url_for, flash
+from website.models import User
+from website.models import Event
+from datetime import datetime
+
+views = Blueprint('views', __name__)
+
+@views.route('/', methods=['POST', 'GET'])
+@login_required
+def home():
+    if request.method == 'POST':
+        content = request.form.get('content')
+        if len(content) < 1:
+            flash('Event Is Too Short!', category='error')
+        else:
+            new_event = Event(content=content,user_id=current_user.id)
+            db.session.add(new_event)
+            db.session.commit()
+            flash('Event added Successfully!',category='successfull')
+    
+    return render_template('home.html', user=current_user)
+
+
+@views.route('/delete/<int:id>', methods=['POST', 'GET'])
+@login_required
+def delete(id):
+    #check if user is logged in
+
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    else:
+        delete = Event.query.get_or_404(id)
+        if delete:
+            if delete.user_id != current_user.id:
+                abort(403)
+            else:
+                    db.session.delete(delete)
+                    db.session.commit()
+                    flash('Event deleted successfully', category='successfull')
+                    return redirect(url_for('views.home'))
+@views.route('/update/<int:id>', methods=['POST', 'GET'])
+@login_required
+def update(id):
+    # task_id = task.query.get_or_400(id)
+    # if request.method =='POST':
+        content = request.form.get('content')
+        if len(content) < 1  or not content:
+            flash('conent is require', category='error')
+        elif len(content) > 50:
+            flash('Error the content is too long', category='error')
+        else:
+            return(render_template('update.html'))
+            event.content=content
+            db.session.commit()
+            flash('Event updated successfully', category='successfull') # i need to use try and except****
+            return redirect(url_for('views.home'))
+#this route accept id as parameter
